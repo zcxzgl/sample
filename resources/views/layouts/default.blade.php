682fe51ee6fce1e7997a16cb99e5f42241cf2617
ChangXin <2422527665@qq.com>
2018-05-14 03:10:39 +0000

    Finish layout add routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Sample App')</title>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-  	<!--该占位区域将用于显示 content 区块的内容，而 content 区块的内容将由继承自 default 视图的子视图定义。-->
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
   </body>
 </html>
