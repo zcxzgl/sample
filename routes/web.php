909d094443f119d538e9bf2df6be8cb0b749a45f
ChangXin <2422527665@qq.com>
2018-07-17 02:08:56 +0000

    Add user statuses

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', 'StaticPagesController@home');
 Route::get('/home', 'StaticPagesController@home')->name('home');
 Route::get('/about', 'StaticPagesController@about')->name('about');
-Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/help', 'StaticPages7Controller@help')->name('help');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
@@ -30,7 +30,12 @@ Route::get('signup/confirm/{token}','UsersController@confirmEmail')->name('confi
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 //邮箱发送重设链接
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+
 //密码更新页面
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+
 //执行密码更新操作
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+//微博的创建和删除
+Route::resource('statuses','StatusesController',['only'=>['store','destroy']]);
